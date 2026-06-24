da8b9a0354115d20eaf1c47abd80f61c47fc1a53
yasir5405 <yasirnaseem5415@gmail.com>
2026-06-24 18:45:23 +0530

    FIx

---
+++ app/bfhl/route.ts
@@ -0,0 +1,16 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders,
+  });
+}
+
+export async function POST() {
+  return Response.json({ message: "API working" }, { headers: corsHeaders });
+}
